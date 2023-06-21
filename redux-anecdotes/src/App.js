e0991f261a0a6dee997cbe1f0f0c3b6be886d1cd
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-21 16:31:28 +0300

    Complete 6.14

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes =>
+        dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <AnecdoteList />
--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,14 +1,16 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    dispatch(createAnecdote(content))
+    const anecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(anecdote))
     dispatch(setNotification(`you added '${content}'`))
     setTimeout(() => dispatch(removeNotification()), 5000)
   }
