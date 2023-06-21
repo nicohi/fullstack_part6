07722f17a44acf59aec717ac46ca59d8ba78fb9d
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-06-21 20:51:28 +0300

    Setup thunk

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -3,15 +3,12 @@ import { useDispatch } from 'react-redux'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes =>
-        dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
